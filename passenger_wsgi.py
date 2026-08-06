872083d9974b000641472e0aac3dc2dcb9adc524
ammardaruger <52695879+ammardaruger@users.noreply.github.com>
2026-08-07 03:16:23 +0500

    Initialize WSGI application in passenger_wsgi.py

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+if APP_ROOT not in sys.path:
+    sys.path.insert(0, APP_ROOT)
+
+from main import app
+
+application = app
